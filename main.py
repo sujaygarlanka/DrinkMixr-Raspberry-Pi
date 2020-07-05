57f8f5c91ddf855ca45cad4cdeff99cb86dc2726
Sujay Garlanka <sujay.garlanka@gmail.com>
2020-07-05 12:27:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ def main():
                 else:
                     if should_refresh_screen:
                         display.clear()
-                        display.println("Waiting for an order...")
+                        display.println("Waiting for an test order...")
                         display.displayImage("/home/pi/DrinkMixr-Raspberry-Pi/media/drink.png", x=85, y=40, height=200, width=150)
                         should_refresh_screen = False
             except:
